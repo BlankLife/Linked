657d04ff3ee88be3fa0131ee381ea27ed7fc9614
Chiuk2 <chiuk2@unlv.nevada.edu>
2017-02-25 01:47:01 -0800

    Login w/ local database

---
+++ AndroidLoginAndRegistration/app/src/main/java/info/androidhive/loginandregistration/activity/UserTypeActivity.java
@@ -0,0 +1,92 @@
+package info.androidhive.loginandregistration.activity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import info.androidhive.loginandregistration.R;
+import info.androidhive.loginandregistration.app.AppConfig;
+import info.androidhive.loginandregistration.app.AppController;
+import info.androidhive.loginandregistration.helper.SQLiteHandler;
+import info.androidhive.loginandregistration.helper.SessionManager;
+
+/**
+ * Created by Kyle on 2/20/2017.
+ */
+
+public class UserTypeActivity extends Activity{
+    private Button btnRegUser;
+    private Button btnOwnerUser;
+    private ProgressDialog pDialog;
+    private SessionManager session;
+    private SQLiteHandler db;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+
+        btnRegUser = (Button) findViewById(R.id.btnRegUser);
+        btnOwnerUser = (Button) findViewById(R.id.btnOwnerUser);
+
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        // Session manager
+        session = new SessionManager(getApplicationContext());
+
+        // SQLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // Check if user is already logged in or not
+        if (session.isLoggedIn()) {
+            // User is already logged in. Take him to main activity
+            Intent intent = new Intent(UserTypeActivity.this,
+                    MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        // Link to Reg Register Screen
+        btnRegUser.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        // Link to Owner Register Screen
+        btnOwnerUser.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        OwnerRegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+
+}
