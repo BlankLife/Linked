266504cdf8c9f077f4076afc0498092e990d964e
orenzaj <john.orenza@unlv.edu>
2017-02-28 17:27:39 -0800

    Added Login Authentication via Firebase

--- Layout_Danielle/app/src/main/java/linked/linkedtest/Start_menu.java
+++ Layout_Danielle/app/src/main/java/linked/linkedtest/Start_menu.java
@@ -1,37 +1,99 @@
 package linked.linkedtest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.R.attr.onClick;
+import static android.R.id.button1;
 import static linked.linkedtest.R.id.signupButton;
+import static linked.linkedtest.R.id.usernameField;
 
 public class Start_menu extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
 
-//        Sign up button from main screen
-        Button button1 = (Button) findViewById(R.id.signupButton);
-        button1.setOnClickListener(new View.OnClickListener(){
+        //Initialize Firebase Object
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        //Sign up button from main screen
+        Button registerButton = (Button) findViewById(R.id.signupButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Start_menu.this, CreateAccount.class);
-                startActivity(i);
+                startActivity(new Intent(Start_menu.this, CreateAccount.class));
             }
         });
 
+        //Log in button from main screen
+        Button loginButton = (Button) findViewById(R.id.signinButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (R.id.usernameField != null || R.id.passwordField)
+                    userLogin();
+            }
+        });
     }
 
-    public void checkLogin(View view) {
+    public void userLogin() {
+        final EditText username = (EditText) findViewById(R.id.usernameField);
+        final EditText password = (EditText) findViewById(R.id.passwordField);
 
-    }
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
 
-    public void gotoCreate(View view) {
+        mAuth.signInWithEmailAndPassword(user, pass)
+                .addOnCompleteListener(Start_menu.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        startActivity(new Intent(Start_menu.this, BusinessActivities.class));
 
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(Start_menu.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
+
